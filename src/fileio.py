f78346513adcf856665238ba59cd36c7c786e794
Joy Menten <picodase@atomicmail.io>
2022-11-06 02:34:11 -0800

    Add conditions, migrate functions from ipynb

--- src/fileio.py
+++ src/fileio.py
@@ -1,8 +1,10 @@
 ## IMPORTS
 
-import os
+import os, sys
+import urllib
 import pickle
 import numpy as np
+import pandas as pd
 import pynmrstar
 
 from tqdm import tqdm
@@ -11,6 +13,39 @@ from os.path import exists
 
 ## FUNCTIONS
 
+def search_files(directory='.', extension=''):
+    filelist = []
+
+    extension = extension.lower()
+    for dirpath, dirnames, files in os.walk(directory):
+        for name in files:
+            if extension and name.lower().endswith(extension):
+                filelist.append(os.path.join(dirpath, name))
+            elif not extension:
+                #print(os.path.join(dirpath, name))
+                continue
+    return filelist
+
+def pickle_variable(variable, varname="variable.pkl"):
+    """
+    Pickles a variable into a given filename
+    """
+
+    file = open(varname, 'wb')
+    pickle.dump(variable, file)
+    file.close()
+    return
+
+def unpickle_variable(datafile="variable.pkl"):
+    """
+    Unpickles a variable into a given filename
+    """
+    file = open(datafile, 'rb')
+    variable = pickle.load(file)
+    file.close()
+
+    return variable
+
 def collect_dataset(files:list):
     """
     Collect a dataset from a list of filepaths and save as a pickle archive
@@ -18,8 +53,12 @@ def collect_dataset(files:list):
 
     pdb_ids = []
     chemical_shift_tensors = []
-    temperatures = []
-    pHs = []
+    conditions = {
+        "pH": [],
+        "temperature": []
+    }
+
+    element_sets = []
 
     pdb_id = None
 
@@ -42,16 +81,54 @@ def collect_dataset(files:list):
         pdb_ids.append(pdb_id)
 
         # if the last accession code exists, (just added a PDB code):
-        if len(pdb_id) == 1 :
+        if len(pdb_id) >= 1 :
             # Get the next chemical shift tensor
             tags = ['Comp_index_ID', 'Comp_ID', 'Atom_ID', 'Atom_type', 'Val', 'Val_err']        
             cs_result_sets = [chemical_shift_loop.get_tag(tags) for chemical_shift_loop in entry.get_loops_by_category("Atom_chem_shift")]
             
-            chem_shifts = np.array(cs_result_sets)
-            chemical_shift_tensors.append(chem_shifts)
-            
-    tensors = [t.squeeze() for t in chemical_shift_tensors]
+            chem_shifts = np.array(cs_result_sets)[0]
 
+            #print(chem_shifts)
+
+            #print(chem_shifts.shape)
+
+            df = pd.DataFrame(
+                data=chem_shifts, 
+                columns=["res_idx", "res_id", "atom_type", "element", "chem_shift", "cs_error"]
+            )
+
+            elems = set(df["element"])
+
+            chemical_shift_tensors.append(df)
+
+            element_sets.append(elems)
+
+            condn = []
+
+            tags = ['Type', 'Val']
+            for condition in entry.get_loops_by_category("Sample_condition_variable"):
+                condn.append(condition.get_tag(tags))
+
+
+            ### NOTE: Still need to fix:
+            # - This code DOES NOT resolve by PDB ID, so it just makes a bunch of lists
+
+            for c in condn[0]:
+                label = c[0]
+
+                if label in conditions: 
+                    conditions[label].append(c[1])
+                else: 
+                    conditions[label] = []
+
+    #tensors = [t.squeeze() for t in chemical_shift_tensors]
+
+    pickle_variable(varname="pdb_ids.pkl", variable=pdb_ids)
+    pickle_variable(varname="cs_tensors.pkl", variable=chemical_shift_tensors)
+    pickle_variable(varname="elems.pkl", variable=element_sets)
+    pickle_variable(varname="conditions.pkl", variable=conditions)
+
+    """
     file = open("pdb_ids.pkl", 'wb')
     pickle.dump(pdb_ids, file)
     file.close()
@@ -60,7 +137,12 @@ def collect_dataset(files:list):
     pickle.dump(chemical_shift_tensors, file)
     file.close() 
 
+    file = open("elems.pkl", 'wb')
+    pickle.dump(element_sets, file)
+    file.close()
+    """
     return
+
 
 def load_dataset(dataset_file:str="cs_tensors.pkl") -> list:
     """
@@ -68,14 +150,13 @@ def load_dataset(dataset_file:str="cs_tensors.pkl") -> list:
     """
 
     if exists(dataset_file):
-        file = open(dataset_file, 'rb')
-        chemical_shifts = pickle.load(file)
-
-        file = open("pdb_ids.pkl", 'rb')
-        pdb_ids = pickle.load(file)
+        chemical_shifts = unpickle_variable(datafile=dataset_file)
+        pdb_ids = unpickle_variable(datafile="pdb_ids.pkl")
+        element_sets = unpickle_variable(datafile="elems.pkl")
+        conditions = unpickle_variable(datafile="conditions.pkl")
         
-        print("SUCCESS:\t Datafile loaded from disk")
-        return (chemical_shifts, pdb_ids)
+        print("SUCCESS:\t Datafiles loaded from disk")
+        return (chemical_shifts, pdb_ids, element_sets, conditions)
     else:
         print("ERROR:\t Datafile not loaded. Please load manually")
         return None
